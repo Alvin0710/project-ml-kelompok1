f7a053b4230a2dd6dabdf209b7f13e58ef51e4c5
Alvin0710 <irwantoalvin@gmail.com>
2021-12-12 19:27:33 +0700

    change to php

---
+++ templates/home.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <title>Plot Listing HTML5 Website Template</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Additional CSS Files -->
+  <link rel="stylesheet" href="../assets/css/fontawesome.css">
+  <link rel="stylesheet" href="../assets/css/templatemo-plot-listing.css">
+  <link rel="stylesheet" href="../assets/css/animated.css">
+  <link rel="stylesheet" href="../assets/css/owl.css">
+  <!--
+
+TemplateMo 564 Plot Listing
+
+https://templatemo.com/tm-564-plot-listing
+
+-->
+</head>
+
+<body>
+
+
+
+  <div class="main-banner">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="top-text header-text">
+            <h6>Kelompok 1</h6>
+            <h2>Hoax Detection</h2>
+          </div>
+        </div>
+        <div class="col-lg-12">
+
+          <form id="search-form" action="{{ url_for('predict')}}" method="POST">
+            <div class="row">
+
+              <div class="col-lg-9 align-self-center">
+                <fieldset>
+                  <input type="text" name="input_title" placeholder="Enter the news" autocomplete="on" required>
+                </fieldset>
+              </div>
+
+              <div class="col-lg-3 align-self-right">
+                <fieldset>
+                  <!-- <button class="main-button" type="submit" value="Predict Now"><i class="fa fa-briefcase"></i>Predict Now</button> -->
+                  <input type="submit" , value="predict">
+                </fieldset>
+              </div>
+
+              <!-- <div class="col-lg-3 align-self-right">                        
+                <fieldset>
+                  <i class="fa fa-briefcase"></i><input class="main-button"  type="submit" value="Predict Now">
+                </fieldset>
+              </div> -->
+
+            </div>
+          </form>
+        </div>
+        <div class="col-lg-10 offset-lg-1">
+          <ul class="categories">
+            <li><a href="#"><span class="icon"><img src="../assets/images/search-icon-05.png" alt="Travel"></span> About
+                Us</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <!-- Scripts -->
+  <script src="../vendor/jquery/jquery.min.js"></script>
+  <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets/js/owl-carousel.js"></script>
+  <script src="../assets/js/animation.js"></script>
+  <script src="../assets/js/imagesloaded.js"></script>
+  <script src="../assets/js/custom.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+    crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+    crossorigin="anonymous"></script>
+
+</body>
+
+</html>
